88f0c8f33f24f3565675fd8e120e553c928b10ef
gihandemel <99316257+gihandemel@users.noreply.github.com>
2023-11-03 10:30:00 +1100

    iterate through arrays

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,30 +1,12 @@
-﻿Random random = new();
-int daysUntilExpiration = random.Next(12);
-int discountPercentage = 0;
+﻿int[] inventory = { 200, 450, 700, 175, 250 };
+int sum = 0;
+int bin = 0;
 
-if (daysUntilExpiration == 0)
+foreach (int items in inventory)
 {
-    Console.WriteLine("Your subscription has expired.");
+    sum += items;
+    bin ++;
+    Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
 }
 
-else if (daysUntilExpiration == 1)
-{
-    discountPercentage = 20;
-    Console.WriteLine($"Your subscription expires within a day!");
-}
-
-else if (daysUntilExpiration  <= 5)
-{
-    discountPercentage = 10;
-    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
-}
-
-else if (daysUntilExpiration  <= 10)
-{
-    Console.WriteLine("Your subscription will expire soon. Renew now!");
-}
-
-if (discountPercentage > 0)
-{
-    Console.WriteLine($"Renew now and save {discountPercentage}%!");
-}
+Console.WriteLine($"\n\nThe sum of items is {sum}");
