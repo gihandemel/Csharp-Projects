608168c48fade3340eb62788ed656db79481c5f0
Gihan <gcdemel@gmail.com>
2023-12-22 17:02:18 +1100

    Variable data challenge

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,16 +1,27 @@
-﻿string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+﻿const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
 
-string[] orders = orderStream.Split(',');
-Array.Sort(orders);
+string quantity = "";
+string output = "";
 
-foreach (string order in orders)
-{
-    if (order.Length == 4)
-    {
-        Console.WriteLine(order);
-    }
-    else
-    {
-        Console.WriteLine($"{order}\t- Error");
-    }
-}
+// Your work here
+const string openSpan = "<span>";
+const string closeSpan = "</span>";
+
+int openingPosition = input.IndexOf(openSpan);
+int closingPosition = input.IndexOf(closeSpan);
+
+openingPosition += openSpan.Length;
+
+int length = closingPosition - openingPosition;
+
+quantity = input.Substring(openingPosition, length);
+
+output = input.Remove(0,5);
+output = output.Replace("</div>","");
+output = output.Replace("trade","reg");
+
+
+//
+
+Console.WriteLine($"Quantity: {quantity}");
+Console.WriteLine($"Output: {output}");
